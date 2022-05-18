59d99fa3dc616a92a11bd89e192b042370e4d808
krista-chan <qbotdev84@gmail.com>
2022-05-18 09:43:46 +0100

    Fix transition affecting colour

--- components/core/CoreSidebar.vue
+++ components/core/CoreSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col p-4 border-r-2 dark:border-zinc-700 transition-all" :class="`${collapsed ? 'w-24' : 'w-96'}`">
+  <div class="flex flex-col p-4 border-r-2 dark:border-zinc-700 transition-[width]" :class="`${collapsed ? 'w-24' : 'w-96'}`">
     <div>a</div>
     <div>b</div>
   </div>
