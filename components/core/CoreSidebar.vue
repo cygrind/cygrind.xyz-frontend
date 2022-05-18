dc9e527afc5e42d1eb6101da09d25a0cd3873dff
krista-chan <qbotdev84@gmail.com>
2022-05-18 10:37:54 +0100

    Push latest for remote work

--- components/core/CoreSidebar.vue
+++ components/core/CoreSidebar.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex flex-col p-4 border-r-2 dark:border-zinc-700 transition-[width]" :class="`${collapsed ? 'w-24' : 'w-96'}`">
-    <div>a</div>
-    <div>b</div>
+
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex w-full">
     <div class="flex flex-col p-4">
-      <p v-for="i in 100">a</p>
     </div>
   </div>
 </template>
